7001e2e3b5a2bfbc7dd681f55ef29da754fb8345
sj1999-BIT <e0543446@u.nus.edu.sg>
2021-10-26 21:12:21 +0800

    created attacker.py
    Aim is to separate the offensive functions

---
+++ python-algo/gamelib/attacker.py
@@ -0,0 +1,92 @@
+import random
+
+import gamelib
+
+
+class Attacker:
+
+
+    def __init__(self, config, game_state):
+        self.game_state = game_state
+
+        global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, REMOVE, UPGRADE, STRUCTURE_TYPES, ALL_UNITS, UNIT_TYPE_TO_INDEX, MP, SP
+        UNIT_TYPE_TO_INDEX = {}
+        WALL = config["unitInformation"][0]["shorthand"]
+        UNIT_TYPE_TO_INDEX[WALL] = 0
+        SUPPORT = config["unitInformation"][1]["shorthand"]
+        UNIT_TYPE_TO_INDEX[SUPPORT] = 1
+        TURRET = config["unitInformation"][2]["shorthand"]
+        UNIT_TYPE_TO_INDEX[TURRET] = 2
+        SCOUT = config["unitInformation"][3]["shorthand"]
+        UNIT_TYPE_TO_INDEX[SCOUT] = 3
+        DEMOLISHER = config["unitInformation"][4]["shorthand"]
+        UNIT_TYPE_TO_INDEX[DEMOLISHER] = 4
+        INTERCEPTOR = config["unitInformation"][5]["shorthand"]
+        UNIT_TYPE_TO_INDEX[INTERCEPTOR] = 5
+        REMOVE = config["unitInformation"][6]["shorthand"]
+        UNIT_TYPE_TO_INDEX[REMOVE] = 6
+        UPGRADE = config["unitInformation"][7]["shorthand"]
+        UNIT_TYPE_TO_INDEX[UPGRADE] = 7
+        MP = 1
+        SP = 0
+
+    def stall_with_interceptors(self, game_state):
+        """
+        Send out interceptors at random locations to defend our base from enemy moving units.
+        """
+        # We can spawn moving units on our edges so a list of all our edge locations
+        friendly_edges = game_state.game_map.get_edge_locations(
+            game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
+
+        # Remove locations that are blocked by our own structures
+        # since we can't deploy units there.
+        deploy_locations = self.filter_blocked_locations(friendly_edges, game_state)
+
+        # While we have remaining MP to spend lets send out interceptors randomly.
+        while game_state.get_resource(MP) >= game_state.type_cost(INTERCEPTOR)[MP] and len(deploy_locations) > 0:
+            # Choose a random deploy location.
+            deploy_index = random.randint(0, len(deploy_locations) - 1)
+            deploy_location = deploy_locations[deploy_index]
+
+            game_state.attempt_spawn(INTERCEPTOR, deploy_location)
+            """
+            We don't have to remove the location since multiple mobile 
+            units can occupy the same space.
+            """
+
+    def least_damage_spawn_location(self, game_state, location_options):
+        """
+        This function will help us guess which location is the safest to spawn moving units from.
+        It gets the path the unit will take then checks locations on that path to
+        estimate the path's damage risk.
+        """
+        damages = []
+        # Get the damage estimate each path will take
+        for location in location_options:
+            path = game_state.find_path_to_edge(location)
+            damage = 0
+            for path_location in path:
+                # Get number of enemy turrets that can attack each location and multiply by turret damage
+                damage += len(game_state.get_attackers(path_location, 0)) * gamelib.GameUnit(TURRET,
+                                                                                             game_state.config).damage_i
+            damages.append(damage)
+
+        # Now just return the location that takes the least damage
+        return location_options[damages.index(min(damages))]
+
+    def detect_enemy_unit(self, game_state, unit_type=None, valid_x=None, valid_y=None):
+        total_units = 0
+        for location in game_state.game_map:
+            if game_state.contains_stationary_unit(location):
+                for unit in game_state.game_map[location]:
+                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (
+                            valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
+                        total_units += 1
+        return total_units
+
+    def filter_blocked_locations(self, locations, game_state):
+        filtered = []
+        for location in locations:
+            if not game_state.contains_stationary_unit(location):
+                filtered.append(location)
+        return filtered
