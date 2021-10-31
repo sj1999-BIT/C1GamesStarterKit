0c270d86c0702eb50d61287792bcdfd1caf90bd0
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-10-31 17:41:49 +0800

    defender: implement new strategy

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -99,7 +99,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         attacker = Attacker(self.config)
         observer = Observer(self.config, game_state, self.damaged_turrets, self.dead_turrets,
                             self.omitted_spawn_locations, self.opponent_mp)
-        self.defender.update_state(game_state, self.scored_on_locations, self.damaged_turrets)
+        self.defender.update_state(game_state, self.scored_on_locations, self.damaged_turrets, self.past_history_stored)
         self.attacker.offense_decision(game_state, observer.generate_our_attacker_location(game_state),
                                        observer.min_health_for_attack(game_state), self.past_history_stored)
 
--- python-algo/gamelib/defender.py
+++ python-algo/gamelib/defender.py
@@ -1,3 +1,6 @@
+import random
+
+
 def set_constants(config):
     global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, REMOVE, UPGRADE, STRUCTURE_TYPES, ALL_UNITS, UNIT_TYPE_TO_INDEX
     UNIT_TYPE_TO_INDEX = {}
@@ -29,25 +32,124 @@ class Defender:
         self.game_state = None
         self.scored_on_locations = []
         self.damaged_turrets = {}
+        self.analytics = None
 
-    def update_state(self, game_state, scored_on_locations, damaged_turrets):
+    def update_state(self, game_state, scored_on_locations, damaged_turrets, analytics):
         self.game_state = game_state
+        self.analytics = analytics
         self.scored_on_locations = scored_on_locations
         self.damaged_turrets = list(sorted(list(damaged_turrets.items()), key=lambda x: x[1], reverse=True))
 
-        self.defend_left_right_side()
-        self.defend_center_region()
+        if self.analytics.is_delay_attack_mode:
+            self.build_delay_attack_structures()
 
-        # Now build reactive defenses based on where the enemy scored
-        self.build_reactive_defense()
+        self.defend_frontline_corners()
+        self.defend_frontline_center()
+        self.upgrade_frontline_center()
+        # self.defend_remaining_frontline()
 
-        self.build_more_walls()
+        if self.analytics.is_delay_attack_mode:
+            self.remove_random_wall()
 
-        self.support_damaged_structures()
+        # self.defend_left_right_side()
+        # self.defend_center_region()
+        #
+        # # Now build reactive defenses based on where the enemy scored
+        # self.build_reactive_defense()
+        #
+        # self.build_more_walls()
+        #
+        # self.support_damaged_structures()
+        #
+        # self.build_last_resort_walls()
+        #
+        # self.build_remaining_front_walls()
 
-        self.build_last_resort_walls()
+    def defend_frontline_corners(self):
+        # Build walls first
+        for x in range(0, 3):
+            self.game_state.attempt_spawn(WALL, (x, self.game_state.HALF_ARENA - 1))
+            self.game_state.attempt_spawn(WALL, (self.game_state.ARENA_SIZE - 1 - x, self.game_state.HALF_ARENA - 1))
 
-        self.build_remaining_front_walls()
+        # Build turrets next
+        self.game_state.attempt_spawn(TURRET, (1, self.game_state.HALF_ARENA - 2))
+        self.game_state.attempt_spawn(TURRET, (self.game_state.ARENA_SIZE - 2, self.game_state.HALF_ARENA - 2))
+
+        # Build side walls next
+        self.game_state.attempt_spawn(WALL, (2, self.game_state.HALF_ARENA - 2))
+        self.game_state.attempt_spawn(WALL, (self.game_state.ARENA_SIZE - 3, self.game_state.HALF_ARENA - 2))
+
+        # Upgrade walls last
+        for x in range(0, 3):
+            self.game_state.attempt_upgrade((x, self.game_state.HALF_ARENA - 1))
+            self.game_state.attempt_upgrade((self.game_state.ARENA_SIZE - 1 - x, self.game_state.HALF_ARENA - 1))
+
+    def defend_frontline_center(self):
+        # Build a few walls first
+        for dx in range(3):
+            self.game_state.attempt_spawn(WALL,
+                                          ((self.game_state.ARENA_SIZE - 1) // 2 - dx, self.game_state.HALF_ARENA - 1))
+            self.game_state.attempt_spawn(WALL,
+                                          ((self.game_state.ARENA_SIZE - 1) // 2 + dx, self.game_state.HALF_ARENA - 1))
+
+        # Build turret next
+        self.game_state.attempt_spawn(TURRET, ((self.game_state.ARENA_SIZE - 1) // 2, self.game_state.HALF_ARENA - 2))
+
+        # Build remaining walls last
+        for dx in range(3, 6):
+            self.game_state.attempt_spawn(WALL,
+                                          ((self.game_state.ARENA_SIZE - 1) // 2 - dx, self.game_state.HALF_ARENA - 1))
+            self.game_state.attempt_spawn(WALL,
+                                          ((self.game_state.ARENA_SIZE - 1) // 2 + dx, self.game_state.HALF_ARENA - 1))
+
+    def upgrade_frontline_center(self):
+        # Upgrade frontline center walls
+        for dx in range(6):
+            self.game_state.attempt_upgrade(
+                ((self.game_state.ARENA_SIZE - 1) // 2 - dx, self.game_state.HALF_ARENA - 1))
+            self.game_state.attempt_upgrade(
+                ((self.game_state.ARENA_SIZE - 1) // 2 + dx, self.game_state.HALF_ARENA - 1))
+
+    def defend_remaining_frontline(self):
+        # Build remaining walls
+        for x in range(7, 3, -1):
+            self.game_state.attempt_spawn(WALL, (x, self.game_state.HALF_ARENA - 1))
+            self.game_state.attempt_spawn(WALL, (self.game_state.ARENA_SIZE - 1 - x, self.game_state.HALF_ARENA - 1))
+
+        # Upgrade remaining walls
+        for x in range(8, 3, -1):
+            self.game_state.attempt_upgrade((x, self.game_state.HALF_ARENA - 1))
+            self.game_state.attempt_upgrade((self.game_state.ARENA_SIZE - 1 - x, self.game_state.HALF_ARENA - 1))
+
+    def remove_random_wall(self):
+        while True:
+            x = random.randrange(3, self.game_state.ARENA_SIZE - 3)
+            if not self.game_state.contains_stationary_unit((x, self.game_state.HALF_ARENA - 2)):
+                self.game_state.attempt_remove((x, self.game_state.HALF_ARENA - 1))
+                break
+
+    def build_delay_attack_structures(self):
+        # Build delay attack walls
+        for x in range(7):
+            self.game_state.attempt_spawn(WALL, (8 + x, 5))
+        for x in range(5):
+            self.game_state.attempt_spawn(WALL, (13 + x, 3))
+        for x in range(2):
+            self.game_state.attempt_spawn(WALL, (12 + x, 1))
+
+        # Build left interceptor zone
+        for dx in range(3):
+            self.game_state.attempt_spawn(WALL, (3 + dx, self.game_state.HALF_ARENA - 4))
+        self.game_state.attempt_spawn(WALL, (6, self.game_state.HALF_ARENA - 5))
+        for dy in range(3):
+            self.game_state.attempt_spawn(WALL, (7, self.game_state.HALF_ARENA - 6 - dy))
+
+        # Build right interceptor zone
+        for dx in range(3):
+            self.game_state.attempt_spawn(WALL, (self.game_state.ARENA_SIZE - 4 - dx, self.game_state.HALF_ARENA - 4))
+        self.game_state.attempt_spawn(WALL, (self.game_state.ARENA_SIZE - 7, self.game_state.HALF_ARENA - 5))
+        for dy in range(3):
+            self.game_state.attempt_spawn(WALL, (self.game_state.ARENA_SIZE - 8, self.game_state.HALF_ARENA - 6 - dy))
 
     def defend_left_right_side(self):
         locations = []
